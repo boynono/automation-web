b4f9268b5fa1d44cc19851a5ff10586adbe74705
M Sholeh Wicaksono <msholehw@xl.co.id>
2021-09-28 21:06:03 +0700

    add setup package

--- src/test/java/setups/Hook.java
+++ src/test/java/setups/Hook.java
@@ -1,4 +1,16 @@
 package setups;
 
-public class Hook {
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+public class Hook extends BrowserSetup{
+    @Before
+    public void before() throws Exception {
+        startBrowser();
+    }
+
+    @After
+    public void after() throws Exception {
+        stopBrowser();
+    }
 }
